4bb1242294ea6ba24f90e670bf5980b179f13128
philkatie <kaphillips219@gmail.com>
2022-04-22 17:20:43 -0500

    switching to a real model instead of a fake model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+# class Doll:
+#     def __init__(self, name, haunted, description, age):
+#         self.name = name
+#         self.haunted = haunted
+#         self.description = description
+#         self.age = age
+
+# dolls = [
+#     Doll('Amaya', True, 'desperate to take over your life', 15),
+#     Doll('Persephone', False, 'surprisingly not haunted! but do NOT feed her pomegranite', 1745),
+#     Doll('Chucky', True, 'the original Chucky doll! do not get too close to it', 34),
+#     Doll('Mary Lou', True, 'when my grandma died she became a doll!', 97)
+# ]
+
+class Doll(models.Model):
+    name = models.CharField(max_length=100)
+    haunted = models.BooleanField()
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name 
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Doll
 
 # Create your views here.
-class Doll:
-    def __init__(self, name, haunted, description, age):
-        self.name = name
-        self.haunted = haunted
-        self.description = description
-        self.age = age
-
-dolls = [
-    Doll('Amaya', True, 'desperate to take over your life', 15),
-    Doll('Persephone', False, 'surprisingly not haunted! but do NOT feed her pomegranite', 1745),
-    Doll('Chucky', True, 'the original Chucky doll! do not get too close to it', 34),
-    Doll('Mary Lou', True, 'when my grandma died she became a doll!', 97)
-]
 
 def home(request):
     return HttpResponse('<h1>Welcome to the dollhouse!</h1>')
@@ -23,4 +11,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dolls_index(request):
+    dolls = Doll.objects.all()
     return render(request, 'dolls/index.html', { 'dolls': dolls }) 
