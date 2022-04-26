1ef6abd17e2103ac809c8355748a2a78841e220d
philkatie <kaphillips219@gmail.com>
2022-04-26 15:35:23 -0500

    updating admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Doll, Seance
+from .models import Doll, Seance, Talisman
 
 # Register your models here.
 admin.site.register(Doll)
 admin.site.register(Seance)
+admin.site.register(Talisman)
