61acc500eb12a6f60aedcbe071abd9848d452b21
philkatie <kaphillips219@gmail.com>
2022-04-26 13:38:33 -0500

    adding talisman templates and urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('dolls/<int:pk>/update/', views.DollUpdate.as_view(), name='dolls_update'),
     path('dolls/<int:pk>/delete/', views.DollDelete.as_view(), name='dolls_delete'),
     path('dolls/<int:doll_id>/add_seance/', views.add_seance, name='add_seance'),
+    path('talismans/', views.TalismanList.as_view(), name='talismans_index'),
+    path('talismans/<int:pk>/', views.TalismanDetail.as_view(), name='talismans_detail'),
+    path('talismans/create/', views.TalismanCreate.as_view(), name='talismans_create'),
+    path('talismans/<int:pk>/update/', views.TalismanUpdate.as_view(), name='talismans_update'),
+    path('talismans/<int:pk>/delete/', views.TalismanDelete.as_view(), name='talismans_delete'),
 ] 
